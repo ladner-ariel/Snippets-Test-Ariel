e301eba872b1e5875e46e49b600be9e4fdf78c92
ladner-ariel <anladner.work@gmail.com>
2024-11-06 23:17:06 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,4 +11,4 @@ python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 python manage.py migrate
 
-python -m gunicorn django_snippets.wsgi
+python -m gunicorn django_snippets.wsgi:application -k uvicorn.workers.UvicornWorker
